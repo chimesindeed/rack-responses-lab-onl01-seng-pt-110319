cd4a7fed5c5176c85030c0758f11cfb55813e881
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-24 00:57:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,15 @@
-def call(env)
-  resp = Rack::Response.new
+class Application
+  def call(env)
+    resp = Rack::Response.new
+  
     now = Time.now
+    
     if now.hour >=12
       resp.write "Good Afternoon"
     else
       resp.write "Good Morning"
     end
+    
     resp.finish
   end
 
